2b710ee66462b715185df9fe4d673ac489ac9d42
snow6oy <snow6oy@gmail.com>
2014-09-20 15:31:41 +0100

    added cjlib client and decorated responses

--- models/ListModel.php
+++ models/ListModel.php
@@ -8,15 +8,28 @@ include 'MysqlIO.php';
 
 class ListModel extends MysqlIO {
   function listCollectionJsonData($control) {
+    $items = array();
 
     $sql = <<<SQL
-SELECT CONCAT('http://api.fnarg.net/tsunamis/', ID) as href
+SELECT CONCAT('http://api.fnarg.net/tsunamis/', ID) as href,
+  YEAR as year,
+  COUNTRY as country
 FROM events 
 WHERE YEAR <= ?
 ORDER BY ID DESC 
 LIMIT 0 , ?
 SQL;
-    $items = $this->getBoundRows($sql, $control);
+    $rows = $this->getBoundRows($sql, $control);
+    foreach($rows as $r){
+      array_push($items, 
+        array('href'=>$r['href'], 'data'=>
+          array(
+            array('name'=>'year', 'value'=>$r['year']),
+            array('name'=>'country', 'value'=>$r['country'])
+          )
+        )
+      );
+    }
     return array('items' => $items);
   }
 
